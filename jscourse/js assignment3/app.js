874e8544e0e5d3529c47396827d8ee4e47d90dde
Alex Hale <59656730+haleboat@users.noreply.github.com>
2020-02-12 21:17:14 -0700

    js work

--- jscourse/js assignment3/app.js
+++ jscourse/js assignment3/app.js
@@ -1,50 +1,102 @@
+document.getElementById('load').addEventListener('click', initialLoad)
+// button.addEventListener('click', entryLoad(button.value))
+
 const originalOneFifty = 151
+const url = "https://pokeapi.co/api/v2/pokedex/1"
 
-function createPokemonListHTML(data) {
+function initialLoad() {
 
-  // make card div
-  const card = document.createElement("ul")
-  card.className = "pokemon-list"
+  document.querySelector("#load").remove()
+  showLoading()
 
-  // make entry number
+  fetch(url)
+    .then(x => x.json())
+    .then(data => {
+      const list = data.pokemon_entries
+
+      const ul = pokedexList = document.createElement("ul")
+      pokedexList.className = "pokemon-list"
+
+      removeLoading()
+
+      list.forEach(index => {
+        if (index.entry_number <= originalOneFifty) {
+          createPokemonListHTML(index, ul)
+        }
+      });
+
+    })
+    .catch(err => {
+      showError()
+    })
+}
+
+function entryLoad(pokemonUrl) {
+  fetch(pokemonUrl)
+    .then(x => x.json())
+    .then(data => {
+      const pokemon = data
+      // removeLoading()
+
+      // createPokemonData(pokemon)
+      console.log(pokemon)
+
+    })
+    .catch(err => {
+      // showError()
+    })
+}
+
+// function createListHTML() {
+//   const pokedexList = document.createElement("ul")
+//   pokedexList.className = "pokemon-list"
+
+//   return pokedexList
+// }
+
+function createPokemonListHTML(data, ul) {
   const pokedexNum = data.entry_number
-  const entryNum = document.createElement("li")
+  const pokemonName = data.pokemon_species.name
+  const pokemonURL = data.pokemon_species.url
+
+  const entryNumRow = document.createElement("li")
   if (pokedexNum < 10) {
-    entryNum.textContent = `#00${pokedexNum}`
+    entryNumRow.textContent = `#00${pokedexNum}`
   } else if (pokedexNum >= 10 && pokedexNum < 100) {
-    entryNum.textContent = `#0${pokedexNum}`
+    entryNumRow.textContent = `#0${pokedexNum}`
   } else if (pokedexNum >= 100) {
-    entryNum.textContent = `#${pokedexNum}`
+    entryNumRow.textContent = `#${pokedexNum}`
   }
 
-  // make pokemon name
-  // capitalize in CSS with:
-  //                        text - transform: capitalize;
-  const pokemon = data.pokemon_species.name
-  const pokeName = document.createElement("li")
-  pokeName.textContent = pokemon
+  const pokemonNameRow = document.createElement("li")
+  pokemonNameRow.textContent = pokemonName
 
-  // make link to pokemon
-  const link = data.pokemon_species.url
-  const pokemonURL = document.createElement('li')
-  const viewButton = document.createElement('a')
-  pokemonURL.appendChild(viewButton)
+  const buttonRow = document.createElement('li')
+  const viewButton = document.createElement('button')
 
-  viewButton.href = link
-  viewButton.textContent = `View`
+  viewButton.value = pokemonURL
+  viewButton.textContent = `view`
+  viewButton.id = 'view'
+  // viewButton.addEventListener('click', entryLoad(button.value))
+  buttonRow.appendChild(viewButton)
 
-  // attach entry number to card
-  card.appendChild(entryNum)
 
-  // attach pokemon name to card
-  card.appendChild(pokeName)
+  // attach entry number <li> to <ul>
+  ul.appendChild(entryNumRow)
 
-  // attach link to pokemon to card
-  card.appendChild(pokemonURL)
+  // attach pokemon name <li> to <ul>
+  ul.appendChild(pokemonNameRow)
 
-  // attach card to page
-  document.body.appendChild(card)
+  // attach button to view pokemon <li> to <ul>
+  ul.appendChild(buttonRow)
+
+  // attach ul to document
+  document.querySelector('.container').appendChild(ul)
 }
+
+// function createPokemonData(pokeData) {
+//   console.log(pokeData)
+// }
 
 function showLoading() {
   const loading = document.createElement("p")
@@ -61,34 +113,55 @@ function removeLoading() {
 function showError() {
   const loading = document.createElement("p")
   loading.id = "js-error"
-  loading.textContent = "The news stories seem to be unavailable right now :S"
+  loading.textContent = "There was an error loading the data."
   document.body.appendChild(loading)
 }
 
-function intialLoad() {
-  const url = "https://pokeapi.co/api/v2/pokedex/1"
+// function initialLoad() {
+// const url = "https://pokeapi.co/api/v2/pokedex/1"
 
-  showLoading()
+// showLoading()
 
-  fetch(url)
-    .then(x => x.json())
-    .then(data => {
-      const list = data.pokemon_entries
-      removeLoading()
+// fetch(url)
+//   .then(x => x.json())
+//   .then(data => {
+//     const list = data.pokemon_entries
+//     // removeLoading()
 
-      list.forEach(index => {
-        if (index.entry_number <= originalOneFifty) {
-          createPokemonListHTML(index)
-        }
-      });
+//     list.forEach(index => {
+//       if (index.entry_number <= originalOneFifty) {
+//         createPokemonListHTML(index)
+//       }
+//     });
 
-    })
-    .catch(err => {
-      showError()
-    })
+//   })
+//   .catch(err => {
+//     // showError()
+//   })
+// }
 
-}
 
-function entryLoad() {
+// trying old for loop to only return li's
+// for (let i = 0; i <= originalOneFifty; i++) {
+  //   console.log(data[i])
+  //   const pokedexNum = data[i - 1].entry_number
+  //   const pokemonName = data[i - 1].pokemon_species.name
+  //   const pokemonURL = data[i - 1].pokemon_species.url
 
-}
+  //   let entryNumRow = ''
+
+    // if (pokedexNum < 10) {
+    //   entryNumRow = `#00${pokedexNum}`
+    // } else if (pokedexNum >= 10 && pokedexNum < 100) {
+    //   entryNumRow = `#0${pokedexNum}`
+    // } else if (pokedexNum >= 100) {
+    //   entryNumRow = `#${pokedexNum}`
+    // }
+
+  //   const li = ` <li>${entryNumRow}</li>
+  //                 <li>${pokemonName}</li>
+  //                 <li><button value=${pokemonURL} id='view'>View</button></li>
+  //               `
+
+  //   return li
+  // }
