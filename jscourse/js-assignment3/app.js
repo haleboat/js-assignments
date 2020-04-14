65f63b0ad7ca0b4f345ebbefde2c1202e4bc1537
Alex Hale <59656730+haleboat@users.noreply.github.com>
2020-04-14 12:14:47 -0600

    refactoring

--- jscourse/js-assignment3/app.js
+++ jscourse/js-assignment3/app.js
@@ -74,13 +74,18 @@ function hideList() {
 }
 
 function hideCard() {
-  const card = document.querySelector('.pokemon-card')
-  card.classList = 'pokemon-card hide'
+  resetCard();
+  const card = document.querySelector('.toggle')
+  card.classList = 'toggle hide'
+  const shader = document.querySelector('body')
+  shader.classList = ''
 }
 
 function showCard() {
-  const card = document.querySelector('.pokemon-card')
-  card.classList = 'pokemon-card show'
+  const card = document.querySelector('.toggle')
+  card.classList = 'toggle show'
+  const shader = document.querySelector('body')
+  shader.classList = 'shader'
 }
 
 function createHeader() {
@@ -94,25 +99,15 @@ function createHeader() {
 function createPokemonListHTML(data, ul) {
   const pokedexNum = data.entry_number
   const pokedexName = data.pokemon_species.name
-
-
   const row = document.createElement('li')
-
-
   const entryNum = document.createElement("p")
-  if (pokedexNum < 10) {
-    entryNum.textContent = `#00${pokedexNum}`
-  } else if (pokedexNum >= 10 && pokedexNum < 100) {
-    entryNum.textContent = `#0${pokedexNum}`
-  } else if (pokedexNum >= 100) {
-    entryNum.textContent = `#${pokedexNum}`
-  }
-
-  const pokemonName = document.createElement("p")
-  pokemonName.textContent = pokedexName
-
+  const pokemonName = document.createElement('p')
   const viewButton = document.createElement('button')
 
+
+  row.className = pokedexNum
+  entryNum.textContent = zeroPadding(pokedexNum)
+  pokemonName.textContent = pokedexName
   viewButton.value = `https://pokeapi.co/api/v2/pokemon/${pokedexName}/`
   viewButton.textContent = `view`
   viewButton.id = 'view'
@@ -123,19 +118,10 @@ function createPokemonListHTML(data, ul) {
   }
 
   viewButton.addEventListener('click', loadEntry)
-
-  // attach entry number <li> to <ul>
   row.appendChild(entryNum)
-
-  // attach pokemon name <li> to <ul>
   row.appendChild(pokemonName)
-
-  // attach button to view pokemon <li> to <ul>
   row.appendChild(viewButton)
-
   ul.appendChild(row)
-
-  // attach ul to document
   document.querySelector('.container').appendChild(ul)
 }
 
@@ -144,30 +130,40 @@ function createPokemonCard(pokeData) {
   const name = pokeData.name
   const sprite = pokeData.sprites.front_default
   const speciesURL = pokeData.species.url
+  const entryID = document.querySelector('#poke-num')
+  const nameID = document.querySelector('#poke-name')
+  const typeID = document.querySelector('#poke-type')
+  const typeIDtwo = document.querySelector('#poke-type2')
+  const image = document.querySelector('.image')
   callSpeciesAPI(speciesURL)
 
-  const entryID = document.querySelector('#poke-num')
-  if (id < 10) {
-    entryID.textContent = `00${id}`
-  } else if (id >= 10 && id < 100) {
-    entryID.textContent = `0${id}`
-  } else if (id >= 100) {
-    entryID.textContent = `${id}`
-  }
 
-  const nameID = document.querySelector('#poke-name')
+  entryID.textContent = zeroPadding(id)
   nameID.textContent = name
-
-  const typeID = document.querySelector('#poke-type')
   typeID.textContent = pokeData.types[0].type.name
-  const typeIDtwo = document.querySelector('#poke-type2')
   typeIDtwo.textContent = ''
   if (pokeData.types.length > 1) {
     typeIDtwo.textContent = pokeData.types[1].type.name
   }
-
-  const image = document.querySelector('.image')
   image.style = `background-image: url(${sprite})`
+}
+
+function zeroPadding(id) {
+  if (id < 10) {
+    return `#00${id}`
+  } else if (id >= 10 && id < 100) {
+    return `#0${id}`
+  } else if (id >= 100) {
+    return `#${id}`
+  }
+}
+
+function resetCard() {
+  document.querySelector('#poke-num').textContent = ''
+  document.querySelector('#poke-name').textContent = ''
+  document.querySelector('#poke-type').textContent = ''
+  document.querySelector('#poke-type2').textContent = ''
+  document.querySelector('.image').style = `background-image: none`
 }
 
 function showLoading() {
@@ -175,7 +171,7 @@ function showLoading() {
   loading.id = "js-loading"
   loading.className = "loading"
   loading.textContent = "Loading..."
-  document.querySelector('.container').appendChild(loading)
+  document.querySelector('.pokemon-card').appendChild(loading)
 }
 
 function removeLoading() {
